0246bae98d6bd4ec8b7a2f7942fd6df8390004fe
Angel <angelgutierrezm17@gmail.com>
2023-12-01 14:43:31 -0300

    Correción en la importación de Favs en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Footer from "./Components/Footer";
 import Navbar from "./Components/Navbar";
 import Home from "./Routes/Home";
-import Favs from "./Routes/favs";
+import Favs from "./Routes/Favs";
 import Detail from "./Routes/Detail";
 import Contact from "./Routes/Contact";
 
